17e4b1fea0672a66cfd2fbc02b3f7907e0a45880
mgraupe <michael.graupner@parisdescartes.fr>
2020-06-07 14:48:27 +0100

    first commit

---
+++ getWaterLevel.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+TRIG = 11
+ECHO = 13
+
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(TRIG,0)
+
+GPIO.setup(ECHO,GPIO.IN)
+
+time.sleep(0.1)
+
+print('starting measurement ...')
+
+GPIO.output(TRIG,1)
+time.sleep(0.00001)
+GPIO.output(TRIG,0)
+
+while GPIO.input(ECHO) == 0:
+    pass
+start = time.time()
+
+while GPIO.input(ECHO) == 1:
+    pass
+stop = time.time()
+
+dist = (stop-start)*17150
+
+print('Distance is :',dist,' cm')
+
+GPIO.cleanup()
+
