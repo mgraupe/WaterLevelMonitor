bfde37d1ccdd78f6c507887561096fb006233092
Michael Graupner <michael.graupner@parisdescartes.fr>
2020-06-13 16:24:30 +0200

    repeated measurements

--- getWaterLevel.py
+++ getWaterLevel.py
@@ -6,30 +6,37 @@ GPIO.setmode(GPIO.BOARD)
 TRIG = 11
 ECHO = 13
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(TRIG,0)
+def measurement():
+    GPIO.setup(TRIG,GPIO.OUT)
+    GPIO.setup(TRIG,0)
 
-GPIO.setup(ECHO,GPIO.IN)
+    GPIO.setup(ECHO,GPIO.IN)
 
-time.sleep(0.1)
+    time.sleep(0.1)
 
-print('starting measurement ...')
+    print('starting measurement ...')
 
-GPIO.output(TRIG,1)
-time.sleep(0.00001)
-GPIO.output(TRIG,0)
+    GPIO.output(TRIG,1)
+    time.sleep(0.00001)
+    GPIO.output(TRIG,0)
 
-while GPIO.input(ECHO) == 0:
-    pass
-start = time.time()
+    while GPIO.input(ECHO) == 0:
+        pass
+    start = time.time()
 
-while GPIO.input(ECHO) == 1:
-    pass
-stop = time.time()
+    while GPIO.input(ECHO) == 1:
+        pass
+    stop = time.time()
 
-dist = (stop-start)*17150
+    dist = (stop-start)*17150
 
-print('Distance is :',dist,' cm')
+    print('Distance is :',dist,' cm')
 
+
+
+while True:
+    measurement()
+    time.sleep(0.5)
+    
 GPIO.cleanup()
 
