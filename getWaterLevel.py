da290525bbda7e6429a4437afeca3bdb7113716f
Michael Graupner <michael.graupner@parisdescartes.fr>
2020-06-14 22:13:09 +0200

    added repeated measurements and data is saved to file

--- getWaterLevel.py
+++ getWaterLevel.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
 
 GPIO.setmode(GPIO.BOARD)
 
 TRIG = 11
 ECHO = 13
 
+#################################################
 def measurement():
     GPIO.setup(TRIG,GPIO.OUT)
     GPIO.setup(TRIG,0)
@@ -13,8 +15,8 @@ def measurement():
     GPIO.setup(ECHO,GPIO.IN)
 
     time.sleep(0.1)
-
-    print('starting measurement ...')
+    if verbose :
+        print('starting measurement ...')
 
     GPIO.output(TRIG,1)
     time.sleep(0.00001)
@@ -29,14 +31,29 @@ def measurement():
     stop = time.time()
 
     dist = (stop-start)*17150
+    if verbose:
+        print('Distance is :',dist,' cm')
 
-    print('Distance is :',dist,' cm')
+###############################################
+Nmeasurements = 50
+verbose = True
 
 
+now = datetime.now()
 
-while True:
-    measurement()
+depth = []
+for i in range(Nmeasurements):
+    d = measurement()
+    depth.append(d)
     time.sleep(0.5)
-    
+
+depth = np.asarray(depth)
+currentDepth = np.median(depth)
+
+# write data to file
+dFile = open("/home/pi/waterLevel/waterLevel.data","a")
+dFile.write("%s %s %s\n" % (now.strftime("%Y-%m-%d"),now.strftime("%H-%M-%S"),depth))
+dFile.close()
+
 GPIO.cleanup()
 
