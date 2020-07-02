cbfedf6f9e7d94eb7069b874e5e2e5e2e77e001e
Michael Graupner <michael.graupner@parisdescartes.fr>
2020-07-02 23:49:14 +0200

    saving is an input argument

--- scrapeRainfall.py
+++ scrapeRainfall.py
@@ -5,15 +5,29 @@ import pdb
 from datetime import datetime
 import os
 
+try:
+    sys.argv[1]
+except:
+    saveData = False
+else:
+    if sys.argv[1] == 'save':
+        saveData = True
+        print('Data will be saved.')
+        print(saveData)
+
+
 now = datetime.now()
 fullHour = now.strftime("%Hh00")
-saveData = True
 scriptWD = os.path.dirname(os.path.realpath(__file__))
 
 # scrape tables from infoclimat website
+# water fall data is from weather station : Paris 6ème - Saint Germain des Prés
+# Département 75 Paris
+# Altitude 60 mètres
+# Coordonnées 48,85°N | 2,34°E
 url = r'https://www.infoclimat.fr/observations-meteo/temps-reel/paris-6eme-saint-germain-des-pres/000CT.html'
 tables = pd.read_html(url) # Returns list of all tables on page
-weatherValues = tables[1] # Select table of interest
+weatherValues = tables[1] # Select table with weather data 
 
 n = 0
 while True:
