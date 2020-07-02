c0d75b2c0b2d16f97f46c4e10d54f3084538aa29
Michael Graupner <michael.graupner@parisdescartes.fr>
2020-07-02 23:43:50 +0200

    rainfall is scraped from infoclimat website

--- scrapeRainfall.py
+++ scrapeRainfall.py
@@ -1,21 +1,37 @@
-import requests
-import xml.etree.ElementTree as ET
+#import requests
+#import xml.etree.ElementTree as ET
+import pandas as pd
+import pdb
+from datetime import datetime
+import os
 
-url = "http://www.infoclimat.fr/public-api/gfs/xml?_ll=48.85341,2.3488&_auth=ABoFEgB%2BBCZQfVBnA3VXflQ8BjMPeQYhVioAY1s%2BB3pROgRlB2dQNlI8B3oFKlBmWHUHZAswAzMBalIqDnxRMABqBWkAawRjUD9QNQMsV3xUegZnDy8GIVY9AG5bKAdnUSwEYQdhUDBSIwdnBTRQZVh0B3gLNQM%2BAWtSNw5mUTsAYwVnAGUEYlAgUC0DNlc2VDIGMg8wBj9WMQBvWzEHNVFnBGkHN1A3UiMHYAUxUGZYYgdjCzUDOgFkUioOfFFLABAFfAAjBCRQalB0Ay5XNlQ5BjI%3D&_c=5ba9a92a4f45fe915fb86afa9c45c7ff"
-response = requests.get(url)
+now = datetime.now()
+fullHour = now.strftime("%Hh00")
+saveData = True
+scriptWD = os.path.dirname(os.path.realpath(__file__))
 
-with open('infoclimat.xml', 'wb') as file:
-	file.write(response.content)
-file.close()
+# scrape tables from infoclimat website
+url = r'https://www.infoclimat.fr/observations-meteo/temps-reel/paris-6eme-saint-germain-des-pres/000CT.html'
+tables = pd.read_html(url) # Returns list of all tables on page
+weatherValues = tables[1] # Select table of interest
 
-tree = ET.parse('infoclimat.xml')
-root = tree.getroot()
-for child in root: 
-	print(child.tag,child.attrib) 
-	if child.tag=='echeance':
-		for subchild in child:
-			#print(subchild.tag,subchild.attrib) 
-			if subchild.tag=='pluie':
-				print(subchild.text)
-				for subsubchild in subchild:
-					print(subsubchild.tag,subsubchild.attrib) 
+n = 0
+while True:
+    hour  = tables[1]['Heure locale'][n]
+    #print(hour, fullHour)
+    if hour == fullHour:
+        pluie = float(weatherValues['Pluie'][n].split()[0])
+        break
+    n+=1
+    
+print('It rained %s mm in the last hour : %s' %(pluie,hour))
+    
+
+if saveData :
+    dFile = open("%s/data/rainfall_%s.data" % (scriptWD,now.strftime("%Y-%m")),"a")
+    dFile.write("%s %s\t%s\n" % (now.strftime("%Y-%m-%d"),now.strftime("%H:00"),pluie))
+    dFile.close()
+    print('rainfall data saved to file')
+
+
+
