9eca5a4827aaba8b67628e2a64e3ba2d9ad9df0a
Michael Graupner <michael.graupner@parisdescartes.fr>
2020-07-14 15:11:08 +0200

    adapted sensor scripts

--- ds18b20.py
+++ ds18b20.py
@@ -7,29 +7,41 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
  
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')
-pdb.set_trace()
-device_file = device_folder + '/w1_slave'
+device_folders = glob.glob(base_dir + '28*')
+#device_file = device_folder + '/w1_slave'
  
-def read_temp_raw():
-    f = open(device_file, 'r')
+def read_temp_raw(dev_file):
+    f = open(dev_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
  
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+def readDS18b20All():
+    temp_c = []
+    for n in range(len(device_folders)):
+        dev_file = device_folders[n] + '/w1_slave'
+        lines = read_temp_raw(dev_file)
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp = float(temp_string) / 1000.0
+            #temp_f = temp_c * 9.0 / 5.0 + 32.0
+        temp_c.append([n,device_folders[n],temp])
+    return temp_c
 	
-while True:
-	print(read_temp())	
-	time.sleep(1)
-	
+#while True:
+#	print(read_temp())
+#	time.sleep(1)
+def main():
+
+    temp_c = readDS18b20All()
+    for n in range(len(temp_c)):
+        print("Senor # : ", n)
+        print("Sensor ID : ", temp_c[n][1])
+        print("Temperature : ", temp_c[n][2], "C")
+
+if __name__=="__main__":
+    main()
