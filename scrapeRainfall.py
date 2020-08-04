c9ff77ce313867acc6c7143bd18be90b3c3dac2f
mgraupe <michael.graupner@parisdescartes.fr>
2020-08-04 18:59:45 +0200

    generalized key search on website

--- scrapeRainfall.py
+++ scrapeRainfall.py
@@ -26,14 +26,16 @@ class scrapeRainfall:
         url = r'https://www.infoclimat.fr/observations-meteo/temps-reel/paris-6eme-saint-germain-des-pres/000CT.html'
         tables = pd.read_html(url) # Returns list of all tables on page
         weatherValues = tables[1] # Select table with weather data
-        pdb.set_tracce()
+        timeKey = [y for y in weatherValues.keys() if 'Heure locale' in y]
+        rainKey = [y for y in weatherValues.keys() if 'Pluie' in y]
+        #pdb.set_trace()
         n = 0
         measurementExists = False
         while n<self.nMaxRows:
-            hour  = weatherValues['\n                Heure locale\n                            '][n]
+            hour  = weatherValues[timeKey[0]][n]
             #print(hour, fullHour)
             if hour == self.fullHour:
-                pluie = float(weatherValues['Pluie'][n].split()[0])
+                pluie = float(weatherValues[rainKey[0]][n].split()[0])
                 measurementExists = True
                 break
             n+=1
